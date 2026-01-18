92302ec9868dda737af8f1f725814e05d7e1f787
Faheem Ahemadji <faheemahemad25@gmail.com>
2026-01-18 16:51:17 +0530

    changes in some files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <>
          <div style={{border:"0px solid blue", overflow:"hidden", height:"100vh",  padding:"5px" }}>
-            <img style={{height:"100%", width:"100%", objectFit:"contain", border:"px solid red"}} src="./public/TechSkills comming soon logo.png" alt="TechoSkills logo"  />
+            <img style={{height:"100%", width:"100%", objectFit:"contain", border:"px solid red"}} src="./TechSkills comming soon logo.png" alt="TechoSkills logo"  />
          </div>
             
         </>
