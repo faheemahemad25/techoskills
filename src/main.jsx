30302f5e2be816da3a94ffae4c1e237ec270a400
Faheem Ahemadji <faheemahemad25@gmail.com>
2026-01-18 17:16:20 +0530

    main.jsx file changed HashRoute bcz child route directly not working

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,28 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// import App from './App.jsx'
+// import { BrowserRouter } from "react-router";
+
+// createRoot(document.getElementById('root')).render(
+//    <BrowserRouter basename="/techoskills/">
+//     <App />
+//   </BrowserRouter>,
+// )
+
+// ------------------------- when child route direct not work then ---------------------
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
+
 
 createRoot(document.getElementById('root')).render(
-   <BrowserRouter basename="/techoskills/">
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>
+
 )
