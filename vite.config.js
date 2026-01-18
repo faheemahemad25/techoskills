f3b0fdacdafa10addd22f307847db1187c262b20
Faheem Ahemadji <faheemahemad25@gmail.com>
2026-01-18 18:17:31 +0530

    delete env file and chnage vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   base: "/techoskills/",
-//   plugins: [react()],
-// });
-
-// ------------------- this is for github and also for vercel -----------
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: import.meta.env.VITE_BASE_PATH || '/',
-// })
-
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
-import { dirname } from 'node:path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+export default defineConfig({
+  base: "/techoskills/",
+  plugins: [react()],
+});
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname, '')
-
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_PATH || '/',
-  }
-})
 
 
 
